e5c860ee3238cdc44744c0416a21f76bfb3a429d
Neha Anand <neha_an@hcl.com>
2017-08-30 13:59:17 +0530

    dictionary

---
+++ dict1.py
@@ -0,0 +1,15 @@
+fruits = {"name1":"orange","name2":"apple","name3":"grapes"}
+print (fruits)
+print (fruits["name2"])
+fruits["name4"]= "banana"
+print(fruits)
+fruit_name = fruits["name1"]
+print(fruit_name)
+
+cars = {}
+print (cars)
+cars["car1"]= "benz"
+print (cars)
+cars["car2"]= "honda"
+print(cars)
+
